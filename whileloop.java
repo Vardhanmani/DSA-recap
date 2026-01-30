dc18d0aee2824ae749e74e92c48d58d6d9e14dbf
mani <pashammanl2@gmail.com>
2026-01-31 01:12:00 +0530

    while loop

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class whileloop{
+    public static void main(String[] args) {
+        int number=1;
+        while(number<=10){
+            System.out.println(number);
+            number++;
+        }
+    }
+}
